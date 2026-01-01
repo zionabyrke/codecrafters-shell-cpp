db08785b5da5a45d77df889aba030b5353c67a78
zionabyrke <renzkirbyonia@gmail.com>
2026-01-01 15:56:07 +0800

    REPL no eval

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  
+  // REPL
+  while (1)
+  {
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+    std::cout << "$ ";
 
-  std::string input = "";
-  std::cin >> input;
-  std::cout << input << ": command not found" << std::endl;
+    std::string input = "";
+    std::cin >> input;
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
