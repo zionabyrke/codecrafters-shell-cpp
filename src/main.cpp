6ba0fa0467cb4c022ebf846009504ddd9d601371
zionabyrke <renzkirbyonia@gmail.com>
2026-01-01 21:53:52 +0800

    echo + refactor

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,23 @@ int main() {
     std::cerr << std::unitbuf;
 
     std::cout << "$ ";
+    std::string command = "";
+    std::string arguments = "";
 
-    std::string input = "";
-    std::cin >> input;
-
-    if (input == "exit")
-    {
+    // parsing: <command> [option] [argument]
+    std::cin >> command;
+    if (command == "exit"){
       break;
     }
-    std::cout << input << ": command not found" << std::endl;
+    // remaining arguments w/o 1st space
+    getline(std::cin >> std::ws, arguments);
+
+    if (command == "echo"){
+      std::cout << arguments << std::endl;
+    }
+    else{
+      std::cout << command << ": command not found" << std::endl;
+    }
   }
 
   return 0;
