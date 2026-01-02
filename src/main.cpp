0786aabcc86ea4d6ffc3799496aead98191229f9
zionabyrke <renzkirbyonia@gmail.com>
2026-01-02 22:18:22 +0800

    reusable funcs made

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,43 @@
 #include <string>
 #include <sstream>
 
+const std::string builtins[] = {"exit", "echo", "type"};
+
+std::string remove_command(std::string input){
+  // POPS the 1st word(command) from input
+    size_t fspace_pos = input.find(' ');
+    if (fspace_pos != std::string::npos){
+      input.erase(0, fspace_pos + 1);
+    }
+    else{
+      input.clear();
+    }
+    return input;
+}
+
+bool is_builtin(std::string command){
+  for(std::string builtin: builtins){
+    if (builtin == command){
+      return true;
+    }
+  }
+  return false;
+}
+
+void echo(std::string msg){
+  std::cout << msg << std::endl;
+}
+
+void type(std::string command){
+  std::cout << command;
+  if (is_builtin(command)){
+    std::cout << " is a shell builtin" << std::endl;
+  }
+  else{
+    std::cout << ": not found" << std::endl;
+  }
+}
+
 int main() {
   
   // REPL
@@ -21,30 +58,17 @@ int main() {
     // command
     std::stringstream ss(input);
     ss >> command;
+    input = remove_command(input);
+
     if (command == "exit"){
       break;
-    }    
-
-    // args: remove command
-    size_t fspace_pos = input.find(' ');
-    if (fspace_pos != std::string::npos){
-      input.erase(0, fspace_pos + 1);
     }
-    else{
-      input.clear();
-    }
-
-    if (command == "echo"){
-      std::cout << input << std::endl;
+    else if (command == "echo"){
+      echo(input);
     }
     else if(command == "type"){
-      std::cout << input;
-      if (input == "exit" || input == "echo" || input == "type"){
-        std::cout << " is a shell builtin" << std::endl;
-      }
-      else{
-        std::cout << ": not found" << std::endl;
-      }
+      ss >> command;
+      type(command);
     }
     else{
       std::cout << command << ": command not found" << std::endl;
