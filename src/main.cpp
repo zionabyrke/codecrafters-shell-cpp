c700da953d53360903aa9da38404e39bff204288
zionabyrke <renzkirbyonia@gmail.com>
2026-01-03 18:23:29 +0800

    type w/ PATH

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <filesystem>
+#include <cstdlib>
+#include <vector>
 
-const std::string builtins[] = {"exit", "echo", "type"};
+#ifdef _WIN32
+  #include <io.h>
+  #define access _access
+  #ifndef X_OK
+    #define X_OK 1
+  #endif
+  constexpr char PATH_LIST_SEPARATOR = ';';
+#else
+  #include <unistd.h>
+  constexpr char PATH_LIST_SEPARATOR = ':';
+#endif
+
+namespace fs = std::filesystem;
+const std::string BUILTINS[] = {"exit", "echo", "type"};
 
 std::string remove_command(std::string input){
   // POPS the 1st word(command) from input
-    size_t fspace_pos = input.find(' ');
-    if (fspace_pos != std::string::npos){
-      input.erase(0, fspace_pos + 1);
-    }
-    else{
-      input.clear();
-    }
-    return input;
+  std::stringstream ss(input);
+  std::string output = "";
+  ss >> output;
+  return output;
 }
 
-bool is_builtin(std::string command){
-  for(std::string builtin: builtins){
+bool is_builtin(const std::string &command){
+  for(const auto& builtin: BUILTINS){
     if (builtin == command){
       return true;
     }
@@ -29,14 +41,44 @@ void echo(std::string msg){
   std::cout << msg << std::endl;
 }
 
-void type(std::string command){
-  std::cout << command;
+bool check_path_exec(const std::string &command, const char *PATH) {
+    if (PATH == nullptr){ 
+      return false;
+    }
+    std::stringstream ss(PATH);
+    std::string dir;
+
+    while (std::getline(ss, dir, PATH_LIST_SEPARATOR)) {
+        if (dir.empty()){
+          continue; // empty directory
+        }
+        fs::path test_dir = fs::path(dir) / command;
+        if (!fs::exists(test_dir)){
+          continue;
+        }
+        if (!fs::is_regular_file(test_dir)){
+          continue;
+        }
+// for linux
+#ifndef _WIN32
+        if (access(test_dir.c_str(), X_OK) != 0) continue;
+#endif
+        std::cout << command << " is " << test_dir.string() << std::endl;
+        return true;
+    }
+    return false;
+}
+
+void type(std::string command, const char* PATH){
   if (is_builtin(command)){
-    std::cout << " is a shell builtin" << std::endl;
+    std::cout << command << " is a shell builtin" << std::endl;
+    return;
   }
-  else{
-    std::cout << ": not found" << std::endl;
+  if (check_path_exec(command, PATH)){
+    return; // success
   }
+  // unsuccessful
+  std::cout << command << ": not found" << std::endl;
 }
 
 int main() {
@@ -47,30 +89,36 @@ int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-
     std::cout << "$ ";
-    std::string input = "";
+
+    // variables
+    const char*PATH = std::getenv("PATH");
+    std::string line = "";
     std::string command = "";
+    std::string args = "";
 
     // parsing: <command> [option] [argument]
-    getline(std::cin, input);
-
+    if (!std::getline(std::cin, line)){
+      break; // input problem
+    }
     // command
-    std::stringstream ss(input);
-    ss >> command;
-    input = remove_command(input);
-
+    std::istringstream iss(line);
+    iss >> command;
+    //args
+    std::getline(iss, args); // remove space
+    if (!args.empty() && args[0] == ' ') args.erase(0, 1);
+    
     if (command == "exit"){
       break;
     }
     else if (command == "echo"){
-      echo(input);
+      echo(args);
     }
     else if(command == "type"){
-      ss >> command;
-      type(command);
+      std::string next_command = remove_command(args);
+      type(next_command, PATH);
     }
-    else{
+    else if (! command.empty()){
       std::cout << command << ": command not found" << std::endl;
     }
   }
